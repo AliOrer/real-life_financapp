ec4f99cf93f72bfcb08adcd96e6a5edc7fb8816d
Ali Örer <129732909+AliOrer@users.noreply.github.com>
2025-10-01 00:24:02 +0200

    js-added

---
+++ 1-app_with_html-js-css/main.js
@@ -0,0 +1,13 @@
+async function getPrice() {
+  try {
+    const res = await 
+    fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd");
+    const data = await res.json();
+    
+    document.getElementById("price").innerText = data.bitcoin.usd + "USD";
+  } catch (error) {
+    document.getElementById("price").innerText = "Hata: " + error.message;
+  }
+}
+
+getPrice();
